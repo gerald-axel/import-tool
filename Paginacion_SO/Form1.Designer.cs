4dcc6c583250abbd1928ced274dd1ebbacc5b299
cruz eduardo cib <cruz.eduardo.cib@gmail.com>
2016-03-21 20:45:14 -0600

    Crea Memoria Princial

--- Paginacion_SO/Form1.Designer.cs
+++ Paginacion_SO/Form1.Designer.cs
@@ -28,12 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Tam = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Tam_Pag = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.num_Pag = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crear Memoria";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tam Memoria";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tam Pagina";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Memoria Principal es de :";
+            // 
+            // Tam
+            // 
+            this.Tam.AutoSize = true;
+            this.Tam.Location = new System.Drawing.Point(166, 107);
+            this.Tam.Name = "Tam";
+            this.Tam.Size = new System.Drawing.Size(0, 13);
+            this.Tam.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MB";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tamaño de Pagina es de : ";
+            // 
+            // Tam_Pag
+            // 
+            this.Tam_Pag.AutoSize = true;
+            this.Tam_Pag.Location = new System.Drawing.Point(166, 132);
+            this.Tam_Pag.Name = "Tam_Pag";
+            this.Tam_Pag.Size = new System.Drawing.Size(0, 13);
+            this.Tam_Pag.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Numero de Paginas es de ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(192, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "MB";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // num_Pag
+            // 
+            this.num_Pag.AutoSize = true;
+            this.num_Pag.Location = new System.Drawing.Point(166, 157);
+            this.num_Pag.Name = "num_Pag";
+            this.num_Pag.Size = new System.Drawing.Size(0, 13);
+            this.num_Pag.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 262);
+            this.Controls.Add(this.num_Pag);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Tam_Pag);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Tam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Tam;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Tam_Pag;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label num_Pag;
     }
 }
 
--- Paginacion_SO/Form1.cs
+++ Paginacion_SO/Form1.cs
@@ -12,9 +12,55 @@ namespace Paginacion_SO
 {
     public partial class Form1 : Form
     {
+        public int TamañoMemoria;
+        public int TamañoPagina;
+        public int tamano;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TamañoMemoria = Convert.ToInt32(textBox1.Text);
+            TamañoPagina = Convert.ToInt32(textBox2.Text);
+            if (TamañoMemoria > TamañoPagina)
+            {
+                tamano = Math.Abs(TamañoMemoria / TamañoPagina);
+            }
+            if (TamañoMemoria < TamañoPagina)
+            {
+                MessageBox.Show("El Tamaño de la pagina ni puede ser mayor que la memoria principal");
+            }
+
+           
+            for (int i = 0; i < tamano; i++)
+            {
+                Button b = new Button();
+                b.Name = "b" + (i * tamano).ToString() + (500).ToString();
+                b.Size = new Size(20, 60);
+                b.BackColor = Color.White;
+                b.FlatStyle = FlatStyle.Flat;
+                b.FlatAppearance.BorderColor = Color.Black;
+                b.Location = new Point(i*20,180);
+                this.Controls.Add(b);
+            }
+
+            Tam.Text = Convert.ToString(TamañoMemoria)+ " MB";
+            Tam_Pag.Text = Convert.ToString(TamañoPagina) + " MB";
+            num_Pag.Text = Convert.ToString(tamano);
+
+
+
+
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
